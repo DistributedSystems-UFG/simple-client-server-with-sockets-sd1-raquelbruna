e434b2fa94c98bcd6218cbe20e5174bf893445dc
Rae Oliveira <72580754+rae-oliveira@users.noreply.github.com>
2023-05-17 21:20:30 -0300

    Update server.py

--- server.py
+++ server.py
@@ -19,5 +19,5 @@ while True:                # forever
     result = data[1][0] * data[1][1]
   elif data[0] == 'division':
     result = data[1][0] / data[1][1]
-  conn.send(pickle.dumps(result)) # return operation
+  conn.send(pickle.dumps(result)) # return operation's result
 conn.close()               # close the connection

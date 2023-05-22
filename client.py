37c07908874b878908b470da0c98d8a4a4656144
santos-bruna <70614304+santos-bruna@users.noreply.github.com>
2023-05-21 22:29:56 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,13 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
 import pickle
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-op = ('add', (10,20))
+s.connect((HOST, PORT))
+
+op = ('add', (10, 20))
 s.send(pickle.dumps(op))
 data = s.recv(1024)
-print(prickle.loads(data))
-s.close()               # close the connection
+print(pickle.loads(data))
+
+s.close()
